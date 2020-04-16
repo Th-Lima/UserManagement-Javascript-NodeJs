5896860ed9c3864343e99d74de1d6e0aaeaa7fdd
Thales Lima <lthales53@gmail.com>
2020-04-16 19:56:28 -0300

    Consuming routes post put delete with Restify

--- client-server/routes/users.js
+++ client-server/routes/users.js
@@ -8,17 +8,61 @@ var router = express.Router();
 //Create a JSON client
 var client = restify.createJsonClient({
   url: 'http://localhost:4000'
-})
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  
-  client.get('/users', function(err, request, response, obj){
-    assert.ifError(err);
     
-    res.end(JSON.stringify(obj, null, 2));
-  });
+    client.get('/users', function(err, request, response, obj){
+      assert.ifError(err);
+      
+      res.json(obj);
+    });
 
+});
+
+  //POST
+  router.get('/:id', function(req, res, next) {
+    
+    client.get(`/users/${req.params.id}`, function(err, request, response, obj){
+      assert.ifError(err);
+      
+      res.json(obj);
+    });
+  
+});
+  
+  //PUT
+  router.put('/:id', function(req, res, next) {
+    
+      client.put(`/users/${req.params.id}`, req.body, function(err, request, response, obj){
+        assert.ifError(err);
+        
+        res.json(obj);
+    });
+  
+});
+ 
+  //DELETE
+  router.delete('/:id', function(req, res, next) {
+    
+        client.del(`/users/${req.params.id}`, function(err, request, response, obj){
+          assert.ifError(err);
+          
+          res.json(obj);
+      });
+    
+});
+  
+    //POST
+    router.post('/', function(req, res, next) {
+    
+          client.post(`/users`, req.body, function(err, request, response, obj){
+            assert.ifError(err);
+            
+            res.json(obj);
+      });
+    
 });
 
 module.exports = router;
